6e07601ec20eb985cc52f1e61a6b6fe015e87c7e
xkylz1 <fauzansaputra741@gmail.com>
2024-06-20 23:40:32 +0800

    update: Home content

--- layouts/header-home.php
+++ layouts/header-home.php
@@ -1,8 +1,16 @@
 <!-- header -->
 <div class="jumbotron jumbotron-fluid jumbotron-home">
-        <div class="container">
-            <h1 class="display-4">Selamat datang di Website Fauzan</h1>
-            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <div class="container d-flex justify-content-end">
+        <div class="col-8">
+            <h1 class="display-4">Hi, I'm Fauzan – A Creative Enthusiast</h1>
+            <p class="lead">
+                Welcome to my portfolio! I'm Fauzan, a passionate creative enthusiast who thrives on transforming ideas into
+                compelling visuals and innovative solutions. With a keen eye for detail and a dedication to artistic
+                excellence, I am committed to pushing the boundaries of creativity in every project I undertake. Whether
+                it's graphic design, digital art, or multimedia production, I am driven by a desire to create meaningful and
+                engaging experiences. Explore my work and discover the vibrant world of creativity I bring to life.
+            </p>
         </div>
     </div>
+</div>
 <!-- header-end -->
