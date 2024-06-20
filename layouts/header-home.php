b15942525b8fa9868143489770b490aa508c6648
xkylz1 <fauzansaputra741@gmail.com>
2024-06-20 23:33:44 +0800

    fix: home image

--- layouts/header-home.php
+++ layouts/header-home.php
@@ -1,7 +1,7 @@
 <!-- header -->
 <div class="jumbotron jumbotron-fluid jumbotron-home">
         <div class="container">
-            <h1 class="display-4">Selamat datang di Website Setyo</h1>
+            <h1 class="display-4">Selamat datang di Website Fauzan</h1>
             <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
         </div>
     </div>
